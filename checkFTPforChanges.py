3e35174bc377c596bf284f44ed1848dbee98204b
Tomás Fartaria <fartaria.tomas@gmail.com>
2016-03-26 20:41:45 +0000

    Checks ftp for changes in a certain file
    Checks ftp for changes in a certain file, saves the file and compares it
    with a previous version. Sends email to new emails on that file

--- checkFTPforChanges.py
+++ checkFTPforChanges.py
@@ -1,37 +1,66 @@
 import ftplib
-
+import os
+import smtplib
 
 def downloadEmailsFTP():
-    connect = ftplib.FTP('ftp.cluster014.ovh.net')
-    connect.login('pvcrops-wp8', 'MoniToriX')
-        if True:
-            gfile = open(avg_data_dir+'\\'+'Average ' + data_files, 'rb')
-            connect.cwd('UEvora/Averages 15 in 15 minutes')
-            #ftp_folder = connect.walk()
-            # if gfile not in ftp_folder:
-            connect.storbinary('STOR '+'Average ' + data_files, gfile)
-            print('file {} sent'.format(data_files))
-            connect.quit()
-        else:
-            print('error in file {}'.format(data_files))
+    connect = ftplib.FTP('ftp.petiscosprobandulho.com')
+    connect.login('ze@petiscosprobandulho.com', 'ZePovinho@2016')
+    if True:
+        #print(connect.getwelcome())
+        #newListEmailsFile = os.getcwd()+'\\oldListEmails.txt'
+        newListEmailsFile = os.getcwd()+'\\ListEmails.txt'
+        connect.retrbinary('RETR listEmails.txt', open(newListEmailsFile, 'wb').write)
+        connect.quit()
+    else:
+        print('error connecting to ftp server')
 
 
 def getNewEmails():
-    pass
+    oldListEmailsFile = os.getcwd()+'\\oldListEmails.txt'
+    listEmailsFile = os.getcwd()+'\\listEmails.txt'
+    #olF = open(oldListEmailsFile, 'r')
+    with open(oldListEmailsFile, 'r') as oldF:
+        oldListEmails = oldF.read()
+    with open(listEmailsFile, 'r') as newF:
+        listEmails = newF.read()
+        with open(oldListEmailsFile, 'w') as oldF:
+            oldF.write(listEmails)
+    oldListEmails = oldListEmails.split('\n')
+    listEmails = listEmails.split('\n')
+    newListEmails = []
+    for customer in listEmails:
+        if customer in oldListEmails:
+            print("{} is already in the list".format(customer))
+        else:
+            newListEmails.append(customer.split(" "))
+    return newListEmails 
 
+def connectGmail(newListEmails):
+    fromAddress = 'petiscosprobandulho@gmail.com'
+    # Credentials (if needed)
+    username = 'petiscosprobandulho'
+    password = 'ZePovinho'
 
-def connectGmail():
-    pass
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    for customer in newListEmails:
+        toAddress = customer[2]
+        msg = "Olá {0} {1}. I did it. I am the boss of python, ftp, gmail and whatever we might need :-p".format(customer[0],customer[1],customer[2])
+        server.sendmail(fromaddr, toaddrs, msg)
+    #closes server
+    server.quit()
 
-
-def sendWelcomeEmailToNewEmails():
+def sendWelcomeEmailToNewEmails(newListEmails):
     connectGmail()
-
+    for customer in newListEmails:
+        print()
 
 def main():
-    EmailsList = downloadEmailsFTP()
-    newEmailsList = getNewEmails(EmailsList)
-    sendWelcomeEmailToNewEmails()
+    #downloadEmailsFTP()
+    newListEmails = getNewEmails()
+    sendWelcomeEmailToNewEmails(newListEmails)
 
 
 if __name__ == "__main__":
